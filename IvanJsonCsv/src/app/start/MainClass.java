8257ad26b9457eaec1077a649bf1938d2156f767
Ivan Velkushanov <velkushanov.ivan@gmail.com>
2019-06-06 19:25:20 +0300

    Bug fixes and functionality improvement
    Added CSV generation functionality. Improved methods for adding json files.

--- IvanJsonCsv/src/app/start/MainClass.java
+++ IvanJsonCsv/src/app/start/MainClass.java
@@ -1,11 +1,52 @@
 package app.start;
 
+import java.util.Scanner;
+
 import json.utilities.JSONUtil;
 
 public class MainClass {
 
-	public static void main(String[] args) {		
-		//JSONUtil.writeReportDefinition("json-files/report-definition.json");
-		JSONUtil.readReportDefinition("json-files/report-definition.json");
+	public static void main(String[] args) {
+		getUserInput(args);
+	}
+
+	public static void getUserInput(String[] args) {
+		Scanner in = new Scanner(System.in);
+		boolean loopContinues = true;
+		System.out.println("Welcome to the employee management application!");
+
+		while (loopContinues) {
+			System.out.println("Type one of the following options and press Enter:");
+			System.out.println("* Write to json file (employees) - |E|");
+			System.out.println("* Read json file (employees) - |e|");
+			System.out.println("* Write report definition - |D|");
+			System.out.println("* Read report definition - |d|");
+			System.out.println("* Generate report - |R|");
+			System.out.println("* Quit - |Q|");
+			System.out.print("Choose option: ");
+			char choice = in.next().charAt(0);
+			switch (choice) {
+			case 'E':
+				JSONUtil.writeToJsonDataFile(args[0]);
+				break;
+			case 'e':
+				JSONUtil.readJsonDataFile(args[0]);
+				break;
+			case 'D':
+				JSONUtil.writeReportDefinition(args[1]);
+				break;
+			case 'd':
+				JSONUtil.readReportDefinition(args[1]);
+				break;
+			case 'R':
+				JSONUtil.generateCSVReport(args[0], args[1], args[2]);
+				break;
+			case 'Q':
+				loopContinues = false;
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
--- IvanJsonCsv/src/json/utilities/JSONUtil.java
+++ IvanJsonCsv/src/json/utilities/JSONUtil.java
@@ -4,6 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 import org.json.simple.JSONArray;
@@ -19,6 +22,11 @@ public final class JSONUtil {
 	private JSONUtil() {
 	}
 
+	/*
+	 * Prompts the user to type in the attributes of an employee and adds that
+	 * employee to already existing (or creates new) json file in the specified
+	 * path that holds array of employees in it.
+	 */
 	public static void writeToJsonDataFile(String filePath) {
 		System.out.println("Welcome to JSON data file writing functionality!");
 
@@ -35,11 +43,11 @@ public final class JSONUtil {
 		while (!inputTaken) {
 			try {
 				System.out.print("Total sales: ");
-				totalSales = in.nextInt();
+				totalSales = Integer.parseInt(in.nextLine());
 				System.out.print("Sales period: ");
-				salesPeriod = in.nextInt();
+				salesPeriod = Integer.parseInt(in.nextLine());
 				System.out.print("Experience multiplier: ");
-				experienceMultiplier = in.nextFloat();
+				experienceMultiplier = Float.parseFloat(in.nextLine());
 				inputTaken = true;
 			} catch (Exception e) {
 				System.out.println("Please try again with the correct values!");
@@ -54,10 +62,12 @@ public final class JSONUtil {
 		employee.put("experienceMultiplier", experienceMultiplier);
 
 		try {
+			// if the file already exists
 			employeesData = (JSONArray) jsonParser.parse(new FileReader(filePath));
 			System.out.println("Adding to an existing file...");
 			employeesData.add(employee);
 		} catch (FileNotFoundException e) {
+			// if there is no such file
 			System.out.println("Creating a new file...");
 			employeesData = new JSONArray();
 			employeesData.add(employee);
@@ -77,21 +87,29 @@ public final class JSONUtil {
 
 	}
 
-	public static void readJsonDataFile(String filePath) {
+	/*
+	 * Reads the json file containing all employees, prints and returns the json
+	 * array contained in it
+	 */
+	public static JSONArray readJsonDataFile(String filePath) {
+		JSONArray employeeData = null;
 		try {
-			JSONArray employeeData = (JSONArray) jsonParser.parse(new FileReader(filePath));
+			employeeData = (JSONArray) jsonParser.parse(new FileReader(filePath));
 			System.out.println(employeeData);
 		} catch (FileNotFoundException e) {
 			System.out.println("File was not found! Try different path!");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return employeeData;
 	}
 
+	/*
+	 * Gets user input and writes the report definition file in the specified
+	 * path
+	 */
 	public static void writeReportDefinition(String filePath) {
 		System.out.println("Welcome to JSON report definition file writing functionality!");
 
@@ -104,11 +122,11 @@ public final class JSONUtil {
 		while (!inputTaken) {
 			try {
 				System.out.print("Top performers treshold: ");
-				topPerformersTreshold = in.nextInt();
+				topPerformersTreshold = Integer.parseInt(in.nextLine());
 				System.out.print("Use experience multiplier (true/false): ");
-				useExperienceMultiplier = in.nextBoolean();
+				useExperienceMultiplier = Boolean.parseBoolean(in.nextLine());
 				System.out.print("Period limit: ");
-				periodLimit = in.nextInt();
+				periodLimit = Integer.parseInt(in.nextLine());
 				inputTaken = true;
 			} catch (Exception e) {
 				System.out.println("Please try again with the correct values!");
@@ -129,17 +147,97 @@ public final class JSONUtil {
 		}
 	}
 
-	public static void readReportDefinition(String filePath) {
+	/*
+	 * Read the report definition json file, prints it and returns it.
+	 */
+	public static JSONObject readReportDefinition(String filePath) {
+		JSONObject reportDefinition = null;
 		try {
-			JSONObject reportDefinition = (JSONObject) jsonParser.parse(new FileReader(filePath));
+			reportDefinition = (JSONObject) jsonParser.parse(new FileReader(filePath));
 			System.out.println(reportDefinition);
 		} catch (FileNotFoundException e) {
 			System.out.println("File was not found! Try different path!");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return reportDefinition;
+	}
+
+	/*
+	 * Read the two json files and generates csv file containing employees that
+	 * have sales period that is equal or less than the periodLimit property and
+	 * have score that is within the top X percent of the results where X is
+	 * defined in report definition
+	 */
+	public static void generateCSVReport(String targetFilePath, String employeeDataFilePath,
+			String reportDefinitionFilePath) {
+		// ArrayList that contains the calculated scores
+		ArrayList<Float> scores = new ArrayList<Float>();
+
+		JSONArray jsonEmployees = readJsonDataFile(employeeDataFilePath);
+		JSONObject reportDefinition = readReportDefinition(reportDefinitionFilePath);
+
+		// 2D ArrayList that will hold the eligible
+		// employees to be included in the report
+		ArrayList<ArrayList<String>> rows = new ArrayList<ArrayList<String>>();
+
+		// Calculating the scores - if useExperienceMultiplier is set to true
+		// the score is multiplied by it and adding it to the list
+		for (int i = 0; i < jsonEmployees.size(); i++) {
+			JSONObject currentEmployee = (JSONObject) jsonEmployees.get(i);
+			float currentScoreNoMultiplier = Float.parseFloat(currentEmployee.get("totalSales").toString())
+					/ Float.parseFloat(currentEmployee.get("salesPeriod").toString());
+			float currentScore = (boolean) reportDefinition.get("useExperienceMultiplier") == true
+					? currentScoreNoMultiplier
+							* Float.parseFloat(currentEmployee.get("experienceMultiplier").toString())
+					: currentScoreNoMultiplier;
+			scores.add(currentScore);
+		}
+
+		// Sorting the scores in descending order
+		// to find the border score depending on the percentage
+		// treshold after which other
+		// employees are discarded
+		Collections.sort(scores, Collections.reverseOrder());
+		float borderScore = scores.get(Math.round(
+				scores.size() * Float.parseFloat(reportDefinition.get("topPerformersTreshold").toString()) / 100));
+
+		//Adding the eligible employees to the list
+		for (int i = 0; i < jsonEmployees.size(); i++) {
+			JSONObject currentEmployee = (JSONObject) jsonEmployees.get(i);
+			if (Integer.parseInt(currentEmployee.get("salesPeriod").toString()) <= Integer
+					.parseInt(reportDefinition.get("periodLimit").toString())) {
+				float currentScoreNoMultiplier = Float.parseFloat(currentEmployee.get("totalSales").toString())
+						/ Float.parseFloat(currentEmployee.get("salesPeriod").toString());
+				float currentScore = (boolean) reportDefinition.get("useExperienceMultiplier") == true
+						? currentScoreNoMultiplier
+								* Float.parseFloat(currentEmployee.get("experienceMultiplier").toString())
+						: currentScoreNoMultiplier;
+				if (currentScore >= borderScore) {
+					rows.add(new ArrayList<String>(
+							Arrays.asList(currentEmployee.get("name").toString(), Float.toString(currentScore))));
+				}
+			}
+		}
+
+		//Writing to the comma separated values file
+		FileWriter csvWriter;
+		try {
+			csvWriter = new FileWriter(targetFilePath);
+			csvWriter.append("Name");
+			csvWriter.append(",");
+			csvWriter.append("Score");
+			csvWriter.append("\n");
+			for (ArrayList<String> row : rows) {
+				csvWriter.append(String.join(",", row));
+				csvWriter.append("\n");
+			}
+			csvWriter.flush();
+			csvWriter.close();
+			System.out.println("File saved.");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
